fad7dc0e2fc254982fbf447aef7ab009cdf9c431
Mayank Chaudhary <mr.singh244411@gmail.com>
2026-06-20 12:35:12 +0530

    WhatsApp button fixed

--- client/src/components/common/FloatingWhatsApp.jsx
+++ client/src/components/common/FloatingWhatsApp.jsx
@@ -3,7 +3,7 @@ const FloatingWhatsApp = () => {
     <a
       href="https://wa.me/+917999215093"
       target="_blank"
-      className="fixed bottom-6 right-6 z-50 rounded-full bg-green-500 px-5 py-3 text-sm font-bold text-white shadow-xl"
+      className="fixed bottom-4 right-6 z-50 rounded-full bg-green-500 px-5 py-3 text-sm font-bold text-white shadow-xl"
     >
       WhatsApp
     </a>
