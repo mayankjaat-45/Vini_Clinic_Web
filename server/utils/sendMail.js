142ccddac44164ca0c36d2da2ac19e544cbc14d8
Mayank Chaudhary <mr.singh244411@gmail.com>
2026-06-25 10:26:32 +0530

    PORT change for sendMail

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -19,13 +19,14 @@ const sendMail = async ({ to, subject, html, replyTo }) => {
       hasPass: !!process.env.MAIL_PASS,
     });
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: Number(process.env.MAIL_PORT),
-      secure: true,
+      host: process.env.MAIL_HOST || "smtp.gmail.com",
+      port: 587,
+      secure: false, // MUST be false for 587
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      requireTLS: true,
     });
 
     console.log("Checking SMTP connection...");
